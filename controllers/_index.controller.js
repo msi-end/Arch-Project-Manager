a5f24c9e5843a5c64f550673fe2454cbb89c10c9
msi-end <aditya01377@gmail.com>
2023-11-29 14:47:07 +0530

    updates

--- controllers/_index.controller.js
+++ controllers/_index.controller.js
@@ -15,7 +15,21 @@ exports.userManager = (req, res) => {
     db.query(query, (err, result, field) => {
         res.send(result)
     })
-}
+} 
+
+// SELECT employee.em_id, employee.name, employee.number, employee.email, employee.lastLoginAt, employee.lastLogoutAt, employee.status
+//     , COUNT(normal_project_employee.emid), normal_project_employee.cat_status
+//        FROM employee
+//         INNER JOIN normal_project_employee ON employee.em_id = normal_project_employee.emid
+//         INNER JOIN normal_project_cat ON normal_project_cat.category_id = normal_project_employee.category_id
+//         GROUP BY   normal_project_employee.emid;
+
+
+
+
+
+
+
 
 exports.finance = (req, res) => {
     const query = ``
