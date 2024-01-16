d9ff9e2bb6e6f07b133745d963375cb70523b458
msi-end <aditya01377@gmail.com>
2024-01-16 17:08:52 +0530

    new updates

--- utils/mailer.js
+++ utils/mailer.js
@@ -6,17 +6,22 @@ const fs = require('fs');
 const transporter = nmailer.createTransport(
     {
         service: "gmail",
-        host: config.host,
-        port: config.port,
+        // host: config.host,
+        // port: config.port,
         // secure: config.secure,
         auth: {
+            // user:config.user,
+            // pass:config.pass
             type: "OAuth2",
-            user: "your.gmail.here@gmail.com",
-            clientId: "Your ClientID Here",
-            clientSecret: "Your Client Secret Here",
-            refreshToken: "Your Refresh Token Here",
-            accessToken: accessToken
-        }
+            user: config.user,
+            clientId: "1023914290850-j72n4fkjm98hc0j32qvagpmjjg3bdsem.apps.googleusercontent.com",
+            clientSecret: "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",
+            refreshToken: "1//04APZ91Gsd_ZpCgYIARAAGAQSNwF-L9IryJhXNwo-hz5GgaYkuPh1qttdtXK84Auuzv_UyQHgF0mJ09yhd0Ula1PrDGMGnTJtELg",
+            accessToken: "a0AfB_byBgQUUtYupAiaG8E94dNH3D4NI0_u7rmmql"
+        },
+        tls: {
+            rejectUnauthorized: false
+          }
     })
 const emailTemplate = (filepath, data) => {
     let fileDate = fs.readFileSync(filepath, 'utf8');
@@ -31,16 +36,16 @@ const EmailNotifier = async (email, subject, text, htmlFile, htmlData) => {
         let template = emailTemplate(htmlFile, htmlData);
         const options = {
             from: config.user,
-            email,
-            subject,
-            text,
+           to: email,
+           subject: subject,
+           text: text,
             html: template
         }
         let sendInfoStatus = await transporter.sendMail(options);
         return sendInfoStatus;
     } catch (err) {
         console.log('Error in Emaial Sender', err)
-        throw new errHandler('503', 'Unable to send the Email, Recheck utils/mailer.js:26')
+        // throw new errHandler('503', 'Unable to send the Email, Recheck utils/mailer.js:26')
     }
 }
 
