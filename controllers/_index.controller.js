6a7f64300ea85a8cd1b92b9440e1222c814ee6a7
msi-end <aditya01377@gmail.com>
2024-05-30 21:35:54 +0530

    updated , removed log

--- controllers/_index.controller.js
+++ controllers/_index.controller.js
@@ -9,7 +9,6 @@ const dataUnity = require('../utils/arrange')
 // ---- All Index routes here ----
 exports.indexDeshboard = async (req, res) => {
     if (req.session.isLoggedIn == true && req.session.role == 'admin') {
-        console.log(req.query)
         const q = `SELECT deals.*, normal_project_cat.category_id,normal_project_cat.npcid, task.task_name, normal_project_cat.cat_status, normal_project_subtask.stask_id, subtask.sub_task_name, normal_project_subtask.stask_status, normal_project_cat.project_status, normal_project_cat.dateofdeadline FROM deals INNER JOIN normal_project_cat ON normal_project_cat.ndeal_id = deals.id INNER JOIN task ON normal_project_cat.category_id = task.task_id LEFT JOIN normal_project_subtask ON normal_project_subtask.ndeal_id = deals.id AND normal_project_subtask.category_id = normal_project_cat.category_id LEFT JOIN subtask ON subtask.sub_task_id = normal_project_subtask.stask_id WHERE deals.id BETWEEN (SELECT MAX(id)-${Number(req.query.to) * 20} FROM deals) AND (SELECT MAX(id)-${Number(req.query.from) * 20} FROM deals) ORDER BY deals.id DESC;`
         await db.query(q, (err, results) => {
             const grouped = {};
