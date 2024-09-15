13c9a10d2ac1509d1383b733524d41c0b374a611
panchanan16 <dekapanchanan16@gmail.com>
2024-09-15 13:09:01 +0530

    one file was remaining as unresolved but now solved

--- controllers/_index.controller.js
+++ controllers/_index.controller.js
@@ -200,13 +200,8 @@ exports.insertNewMiscDeal = async (req, res) => {
 //---normal projects controll-------
 // SELECT deals.id, deals.reference_no, deals.city, deals.deal_name, deals.split, normal_projects_finance.task, task.task_name, normal_projects_finance.totalamount, normal_projects_finance.amount_got, normal_projects_finance.modeofpay FROM normal_projects_finance INNER JOIN deals ON deals.id = normal_projects_finance.ndeal_id INNER JOIN task ON task.task_id = normal_projects_finance.task;
 exports.renderNormalProjectFinance = async (req, res) => {
-<<<<<<< HEAD
-    // if (req.session.isLoggedIn == true && req.session.role == 'admin') {
-        const q = `SELECT deals.id, deals.reference_no, deals.city, deals.deal_name, deals.split, normal_projects_finance.task, task.task_name, normal_projects_finance.totalamount, normal_projects_finance.amount_got, normal_projects_finance.modeofpay FROM normal_projects_finance INNER JOIN deals ON deals.id = normal_projects_finance.ndeal_id INNER JOIN task ON task.task_id = normal_projects_finance.task;`
-=======
     if (req.session.isLoggedIn == true && req.session.role == 'admin') {
         const q = `SELECT deals.id, deals.reference_no, deals.city, deals.deal_name, deals.split, normal_projects_finance.task, task.task_name, normal_projects_finance.totalamount, normal_projects_finance.amount_got, normal_projects_finance.modeofpay FROM normal_projects_finance INNER JOIN deals ON deals.id = normal_projects_finance.ndeal_id INNER JOIN task ON task.task_id = normal_projects_finance.task WHERE deals.id BETWEEN (SELECT MAX(id)-${Number(req.query.to) * 20} FROM deals) AND (SELECT MAX(id)-${Number(req.query.from) * 20} FROM deals) ORDER BY deals.id DESC;`
->>>>>>> main
         await db.query(q, (err, result) => {
             if (!err) {
                 const grouped = {};
