61e19ca593e05a580c7199748181ef7522295222
msiend <mr.roboxe@gmail.com>
2025-08-09 16:54:35 +0530

    new updates of expense section

--- controllers/fm.expense.crud.js
+++ controllers/fm.expense.crud.js
@@ -2,8 +2,8 @@ const db = require('../config/db.config');
 
 
 exports.addExpense = (req, res) => {
-    let q = `INSERT  INTO  expenses(title,remark,amount,date,md_type) VALUE(?,?,?,?,?)`
-    db.query(q, [req.body.title, req.body.remark, req.body.amount, req.body.date, req.body.mode], (err, results) => {
+    let q = `INSERT  INTO  expenses(title,remark,amount,date,md_type,category) VALUE(?,?,?,?,?,?)`
+    db.query(q, [req.body.title, req.body.remark, req.body.amount, req.body.date, req.body.mode, req.body.mode], (err, results) => {
         if (!err) {
             res.status(200).send({ status: true, msg: 'Successfully Expense Added' })
         } else {
@@ -25,7 +25,7 @@ exports.expenseUpdater = (req, res) => {
 
 }
 exports.GetExpensesByMonths = (req, res) => {
-    let query = `SELECT * FROM expenses WHERE date LIKE '%${req.query.m}/${req.query.y}';`
+    let query = `SELECT * FROM expenses WHERE date LIKE '%${req.query.m}/${req.query.y}' ORDER BY id DESC;`
     db.query(query, (err, results) => {
         if (!err) {
             res.status(200).send({ status: true, msg: 'Successfully Expense Updated',data:results })
--- static/js/admin/expense.js
+++ static/js/admin/expense.js
@@ -4,11 +4,9 @@ let ReqURI = {
   updExps: BASE_URL + `/expsUpdate/`,
   getExpsBymonths: BASE_URL + "/getExps",
   NisProjectPaid: location.origin + "/apiv1/nIsProjectPaid",
-  MisProjectPaid: location.origin + "/apiv1/mIsProjectPaid",};
+  MisProjectPaid: location.origin + "/apiv1/mIsProjectPaid",
+};
 
-function hidePopup(event) {
-  document.querySelector(".main-popup").classList.add("hide");
-}
 function AlertNotifier(status, msg, icon) {
   Swal.fire({
     title: status ? "Success" : "Error",
@@ -17,8 +15,94 @@ function AlertNotifier(status, msg, icon) {
     confirmButtonText: "Done",
   });
 }
+function hidePopup(event) {
+  document.querySelector(".main-popup").classList.add("hide");
+}
+
+// OPEN INCOME EXPENSE SELECTOR
+function addEx_IncPopupToggle(e) {
+  document.getElementsByClassName(e)[0].classList.toggle("show");
+}
+
+//ADD INCOME
+function addIncomeForm() {
+  addEx_IncPopupToggle("add-button-child");
+  const maindrop = document.querySelector(`.main-popup`);
+  maindrop.classList.toggle(`hide`);
+  maindrop.innerHTML = "";
+  maindrop.innerHTML = `<div class="add-expense blur hide">
+    <form class="form">
+        <h2>Add an Income</h2>
+        <div class="grid extra-grid">
+            <div class="field">
+                <p class="title">Enter Project</p>
+                <input type="text" name="project" id="project" placeholder="Search Project by Name,Ref ID. etc.">
+                <div>
+
+                </div>
+                <select name="modeOfPays" id="mode" placeholder="Search Project by Name,Ref ID. etc."  >
+                    <option value="cash">Select From last 10 Projects</option>
+                </select>
+            </div>
+            <div class="field">
+                <p class="title">Expense Amount <span>(in &#8377;)</span></p>
+              <select name="modeOfPays" id="mode"  >
+                    <option value="cash">Select Project Phase</option>
+                </select>
+            </div>
+            <div class="field">
+                <p class="title">Mode of Payment</p>
+                <select name="modeOfPays" id="mode">
+                    <option value="cash">Cash</option>
+                    <option value="online">Online</option>
+                </select>
+            </div>
+            <div class="field">
+                <p class="title">Date <span>(DD/MM/YYYY)</span></p>
+                <input type="date" name="date" id="date">
+            </div>
+            <div class="field">
+                <p class="title">Amount <span>(*optional)</span></p>
+                <input type="text" id="amount">
+            </div>
+        </div>
+        <div class="action-btn flex align-center">
+            <button type="button" onclick="addExpense()" class="flex-1">Add</button>
+            <button type ="reset" class="flex-1 delete" onclick="hidePopup(this)">Cancel</button>
+        </div>
+    </form>
+</div>`;
+  const dropDownTarget = document.querySelector(`.add-expense`);
+  dropDownTarget.classList.toggle(`hide`);
+}
+function addincome() {
+  let expAddCtn = document.getElementsByClassName("add-expense")[0];
+  let dataObj = {
+    title: expAddCtn.querySelector("#exp-name").value,
+    amount: expAddCtn.querySelector("#amount").value,
+    mode: expAddCtn.querySelector("#mode").value,
+    remark: expAddCtn.querySelector("#remark").value,
+    date: date_Split(expAddCtn.querySelector("#date").value, "-" || "/", true),
+  };
+  ReqHandler.POST(ReqURI.addExps, dataObj)
+    .then((res) => {
+      if (res.status == true) {
+        AlertNotifier(res.status, res.msg, "success");
+        setTimeout(() => {
+          location.reload();
+        }, 2000);
+      } else {
+        AlertNotifier(res.status, res.msg, "error");
+      }
+    })
+    .catch((err) => {
+      console.log("Error(fn-ExpsAdd):" + err);
+    });
+}
+
 //ADD EXPENSE
 function addExpenseForm() {
+  addEx_IncPopupToggle("add-button-child");
   const maindrop = document.querySelector(`.main-popup`);
   maindrop.classList.toggle(`hide`);
   maindrop.innerHTML = "";
@@ -37,6 +121,7 @@ function addExpenseForm() {
             <div class="field">
                 <p class="title">Mode of Payment</p>
                 <select name="modeOfPays" id="mode">
+                    <option value="">Select Payment Method</option>
                     <option value="cash">Cash</option>
                     <option value="online">Online</option>
                 </select>
@@ -48,6 +133,14 @@ function addExpenseForm() {
             <div class="field">
                 <p class="title">Remarks <span>(*optional)</span></p>
                 <input type="text" id="remark">
+            </div>
+             <div class="field">
+                <p class="title">Category</p>
+                <select name="category" id="category">
+                    <option value="">Select from Categories</option>
+                    <option value="cash">Fuel</option>
+                    <option value="online">Trevel</option>
+                </select>
             </div>
         </div>
         <div class="action-btn flex align-center">
@@ -66,8 +159,8 @@ function addExpense() {
     amount: expAddCtn.querySelector("#amount").value,
     mode: expAddCtn.querySelector("#mode").value,
     remark: expAddCtn.querySelector("#remark").value,
-    date: date_Split(expAddCtn.querySelector("#date").value, "-"||"/", true),
-  };  
+    date: date_Split(expAddCtn.querySelector("#date").value, "-" || "/", true),
+  };
   ReqHandler.POST(ReqURI.addExps, dataObj)
     .then((res) => {
       if (res.status == true) {
@@ -85,14 +178,14 @@ function addExpense() {
 }
 
 //EDIT EXPENSE
-function editExpense(e){
+function editExpense(e) {
   console.log(e);
-  let id = e.parentElement.parentElement.dataset.exps_id;  
+  let id = e.parentElement.parentElement.dataset.exps_id;
   let target = e.parentElement.parentElement;
   const maindrop = document.querySelector(`.main-popup`);
-    maindrop.classList.toggle(`hide`);
-    maindrop.innerHTML = "";
-    maindrop.innerHTML = `<div class="editexpense blur hide">
+  maindrop.classList.toggle(`hide`);
+  maindrop.innerHTML = "";
+  maindrop.innerHTML = `<div class="editexpense blur hide">
         <form class="form">
           <h2>Edit an Expense</h2>
           <div class="grid extra-grid">
@@ -125,15 +218,22 @@ function editExpense(e){
             <button type="reset" class="flex-1 delete" onclick="hidePopup(this)">Cancel</button>
           </div>
         </form>
-      </div>`
-      const dropDownTarget = document.querySelector(`.editexpense`);
-      dropDownTarget.classList.toggle(`hide`);
-      if(e){
-        dropDownTarget.querySelector(`#exp-name`).value = target.querySelector(`.exp-name-data`).innerText;
-        dropDownTarget.querySelector(`#amount`).value = target.querySelector(`.exp-amount-data`).innerText;
-        dropDownTarget.querySelector(`#date`).value = date_Split(target.querySelector(`.exp-date-data`).innerText,"/",false);
-        dropDownTarget.querySelector(`#mode`).value = target.querySelector(`.exp-mode-data`).innerText;
-      }
+      </div>`;
+  const dropDownTarget = document.querySelector(`.editexpense`);
+  dropDownTarget.classList.toggle(`hide`);
+  if (e) {
+    dropDownTarget.querySelector(`#exp-name`).value =
+      target.querySelector(`.exp-name-data`).innerText;
+    dropDownTarget.querySelector(`#amount`).value =
+      target.querySelector(`.exp-amount-data`).innerText;
+    dropDownTarget.querySelector(`#date`).value = date_Split(
+      target.querySelector(`.exp-date-data`).innerText,
+      "/",
+      false
+    );
+    dropDownTarget.querySelector(`#mode`).value =
+      target.querySelector(`.exp-mode-data`).innerText;
+  }
 }
 function updExpense(exp_id) {
   let editCtn = document.querySelector(".editexpense");
@@ -169,8 +269,7 @@ function ChangeExpsByMonths(e) {
       dataCtn.innerHTML = "";
       if (res.status) {
         res.data.forEach((e) => {
-          let html =
-               `<tr data-e_id="${e.id}">
+          let html = `<tr data-e_id="${e.id}" class="expense-border">
                       <td>
                         ${e.id}
                       </td>
@@ -212,7 +311,11 @@ function ChangeExpsByMonths(e) {
         if (res[i].total_price > res[i].amount_got) {
           Ctn = `<p class="notification-alert flex align-center">
                         <span class="text">
-                            Project (${res[i].deal_name}) is completed while payment of ${res[i].total_price - res[i].amount_got} is still pending.
+                            Project (${
+                              res[i].deal_name
+                            }) is completed while payment of ${
+            res[i].total_price - res[i].amount_got
+          } is still pending.
                         </span>
                         <span class="delete">Delete</span>
                     </p>`;
@@ -231,7 +334,11 @@ function ChangeExpsByMonths(e) {
         if (res[i].total_price > res[i].amount_got) {
           Ctn = `<p class="notification-alert flex align-center">
                         <span class="text">
-                             Project (${res[i].sdeal_name}) is completed while payment of ${res[i].total_price - res[i].amount_got} is still pending.
+                             Project (${
+                               res[i].sdeal_name
+                             }) is completed while payment of ${
+            res[i].total_price - res[i].amount_got
+          } is still pending.
                         </span>
                         <span class="delete">Delete</span>
                     </p>`;
